8568872115edabdccf5174eaf7716589fd7e94db
aymane <ayamnehamouch9@gmail.com>
2026-02-17 15:53:05 +0000

    Version1.0

---
+++ ex1_variables.js
@@ -0,0 +1,8 @@
+function infoEtudiant(nom,note){
+    if(note>=10){
+return "Admis"
+    }
+    else{
+        return "Ajournee"
+    }
+}
---
+++ ex3_tableaux.js
@@ -0,0 +1,18 @@
+ let notes=[12,8,15,10,7]
+ notes.sort((a, b) => b - a);
+ let moyenne , meilleurNote , noteSup=[] ;
+ let sum=0 ;
+ console.log(typeof(notes));
+ for (let i = 0; i < notes.length; i++) {
+     sum+=notes[i] ;
+     if(notes[i]>=10) { 
+        noteSup.push(notes[i])
+        
+     }
+    }
+    moyenne=sum/notes.length ;
+    console.log(`la moyenne est : ${moyenne}`);
+    console.log(`le meilleur note est : ${notes[0]}` );
+    console.log(`le  no,bre de notes >=10 est : ${noteSup}` );
+    
+
